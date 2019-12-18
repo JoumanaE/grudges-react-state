076077cda00c9ef7d64ccf6fd25109296e11013f
Joumana El Alaoui <joumana.codes@gmail.com>
2019-12-18 15:38:44 -0600

    adding undo & redo

--- src/Application.js
+++ src/Application.js
@@ -4,8 +4,10 @@ import Grudges from './Grudges';
 import NewGrudge from './NewGrudge';
 import { GrudgeContext } from './GrudgeContext';
 
+
+
 const Application = () => {
-  const { undo, isPast } = useContext(GrudgeContext);
+  const { undo, isPast, redo, isFuture } = useContext(GrudgeContext);
 
   console.log({ undo, isPast });
 
@@ -13,10 +15,10 @@ const Application = () => {
     <div className="Application">
       <NewGrudge />
       <section>
-        <button disabled={!isPast} onClick={undo}>
+        <button className="full-width" disabled={!isPast} onClick={undo}>
           Undo
         </button>
-        <button>Redo</button>
+        <button className="full-width" disabled={!isFuture} onClick={redo}>Redo</button>
       </section>
       <Grudges />
     </div>
